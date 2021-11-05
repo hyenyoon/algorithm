d244839762764a1820cf3a9f0cc0b00b853d469a
hyenyoon <hyenyoon.j@gmail.com>
2021-11-05 16:04:22 +0900

    음계 문제 해결

--- 2920_음계/2920_음계/main.swift
+++ 2920_음계/2920_음계/main.swift
@@ -7,5 +7,45 @@
 
 import Foundation
 
-print("Hello, World!")
 
+struct OrderScale {
+    func startOrderScale() {
+        let pitches = getPitches()
+        var isContained:Bool = true
+        
+        if pitches.count == 8 {
+            for pitch in 1...8 {
+                if pitches.contains(pitch) == false {
+                    isContained = false
+                    break
+                }
+            }
+            if isContained == true {
+                printScale(order: checkOrder(pitches: pitches))
+            }
+        }
+    }
+    
+    func getPitches()-> [Int] {
+        let input = readLine() ?? ""
+        let split = input.split(separator: " ")
+        return split.map{ Int($0.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)) ?? 0 }
+    }
+    func checkOrder(pitches: [Int])->String {
+        let ascendedPitches = Array(1...8)
+        switch pitches {
+        case ascendedPitches:
+            return "ascending"
+        case ascendedPitches.reversed():
+            return "descending"
+        default:
+            return "mixed"
+        }
+    }
+    func printScale(order: String) {
+        print(order)
+    }
+}
+
+var orderScale = OrderScale()
+orderScale.startOrderScale()
