56f658b1b57ff34aec4ec79f4531b01a0c8d75ba
hyenyoon <hyenyoon.j@gmail.com>
2021-11-05 18:38:22 +0900

    아스키코드 문제 해결

---
+++ 11654_아스키 코드/11654_아스키 코드/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  11654_아스키 코드
+//
+//  Created by Hyenyoon Jung on 2021/11/05.
+//
+
+import Foundation
+
+print("Hello, World!")
+
--- 9858_OX퀴즈/9858_OX퀴즈/main.swift
+++ 9858_OX퀴즈/9858_OX퀴즈/main.swift
@@ -7,5 +7,46 @@
 
 import Foundation
 
-print("Hello, World!")
 
+struct OXquiz {
+    func getLineAndQuiz()->[[Character]] {
+        let lines = Int(readLine()!)!
+        
+        var quizzes = [[Character]]()
+        if lines > 0 {
+            for _ in 1...lines {
+                let quiz = readLine() ?? ""
+                quizzes.append(Array(quiz))
+            }
+        }
+        return quizzes
+    }
+    func scoreQuiz(quizzes: [[Character]])->[Int] {
+        var scores = [Int]()
+        for quiz in quizzes {
+            var score:Int = 0
+            var count:Int = 0
+            for ox in quiz {
+                switch ox {
+                case "O":
+                    count += 1
+                    score += count
+                case "X":
+                    count = 0
+                default:
+                    print("Error")
+                }
+            }
+            scores.append(score)
+        }
+        return scores
+    }
+    func printScore(scores: [Int]) {
+        for score in scores {
+                print(score)
+        }
+    }
+}
+
+var myOXquiz = OXquiz()
+myOXquiz.printScore(scores: myOXquiz.scoreQuiz(quizzes: myOXquiz.getLineAndQuiz()))
