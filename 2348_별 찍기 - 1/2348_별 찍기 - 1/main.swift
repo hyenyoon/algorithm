df8abb5f075f5a7c4b3b3406db58ed3aed9f4b9b
hyenyoon <hyenyoon.j@gmail.com>
2021-11-05 16:03:27 +0900

    음계 문제 해결

--- 2348_별 찍기 - 1/2348_별 찍기 - 1/main.swift
+++ 2348_별 찍기 - 1/2348_별 찍기 - 1/main.swift
@@ -7,5 +7,36 @@
 
 import Foundation
 
-print("Hello, World!")
+struct MakeLeftStarTree {
+    func startMakeStarTree() {
+        let lines:Int = getNumOfLines()
+        if lines >= 1 && lines <= 100 {
+            printStarTree(starTree: setStarTree(lines: lines))
+        }
+    }
+    func getNumOfLines() -> Int {
+        return Int(readLine()!)!
+    }
+    func setStarTree(lines: Int) -> [[Character]] {
+        var resultTree = [[Character]]()
+        for line in 1...lines {
+            var tempTree = [Character]()
+            for _ in 1...line {
+                tempTree.append("*")
+            }
+            resultTree.append(tempTree)
+        }
+        return resultTree
+    }
+    func printStarTree(starTree: [[Character]]) {
+        for line in starTree {
+            for star in line {
+                print(star, terminator: "")
+            }
+            print("")
+        }
+    }
+}
 
+var makeLeftStarTree = MakeLeftStarTree()
+makeLeftStarTree.startMakeStarTree()
---
+++ 2920_음계/2920_음계/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  2920_음계
+//
+//  Created by Hyenyoon Jung on 2021/11/05.
+//
+
+import Foundation
+
+print("Hello, World!")
+
