85c6fb7ffed6ae5577b84d8dbe247eda02abce53
hyenyoon <hyenyoon.j@gmail.com>
2021-11-12 13:58:46 +0900

    2번 해결

---
+++ 알고리즘 211112/알고리즘 211112/2. 크레인 인형뽑기 게임.swift
@@ -0,0 +1,44 @@
+//
+//  2. 크레인 인형뽑기 게임.swift
+//  알고리즘 211112
+//
+//  Created by Hyenyoon Jung on 2021/11/12.
+//
+
+import Foundation
+
+func solution(_ board:[[Int]], _ moves:[Int]) -> Int {
+    let numOfCol = board.count
+    var colNum = Array(repeating: 0, count: numOfCol)
+    var pickedDolls = [Int]()
+    var deletedDolls = 0
+    
+    for move in moves {
+        if colNum[move-1] == numOfCol {
+            continue
+        }
+        if board[colNum[move-1]][move-1] == 0 {
+            var n = colNum[move-1]
+            while board[n][move-1] == 0 {
+                if colNum[n+1] != numOfCol {
+                    n += 1
+                } else {
+                    colNum[move-1] = n+1
+                    break
+                }
+            }
+            colNum[move-1] = n
+        }
+        if board[colNum[move-1]][move-1] == pickedDolls.last {
+            pickedDolls.removeLast()
+            deletedDolls += 2
+        } else {
+            pickedDolls.append(board[colNum[move-1]][move-1])
+        }
+        colNum[move-1] += 1
+    }
+    return deletedDolls
+}
+
+
+
