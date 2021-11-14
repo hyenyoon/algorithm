6366e96a8a4a0db42d8de2afd77f52d069a9da7d
hyenyoon <hyenyoon.j@gmail.com>
2021-11-15 02:28:26 +0900

    문제6 해결

--- 알고리즘 211112/알고리즘 211112/6. 약수의 합.swift
+++ 알고리즘 211112/알고리즘 211112/6. 약수의 합.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+
+
+// 처음 작성한 코드
+func solution(_ n:Int) -> Int {
+    var measures = [Int]()
+    var sum = 0
+    
+    if n > 1 {
+        for measure in 1...n/2 {
+            if n % measure == 0 {
+                if !measures.contains(measure) { measures.append(measure) }
+                if !measures.contains(n/measure) { measures.append(n/measure) }
+            }
+        }
+        
+        for measure in measures {
+            sum += measure
+        }
+        
+        return sum
+        
+    } else {
+        return n
+    }
+}
