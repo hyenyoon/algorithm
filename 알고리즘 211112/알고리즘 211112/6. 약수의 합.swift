7c3da31deb3fd115606fe1221657871481e4c41f
hyenyoon <hyenyoon.j@gmail.com>
2021-11-15 02:34:40 +0900

    문제6 코드보완

--- 알고리즘 211112/알고리즘 211112/6. 약수의 합.swift
+++ 알고리즘 211112/알고리즘 211112/6. 약수의 합.swift
@@ -10,7 +10,7 @@ import Foundation
 
 
 // 처음 작성한 코드
-func solution(_ n:Int) -> Int {
+func solution6_1(_ n:Int) -> Int {
     var measures = [Int]()
     var sum = 0
     
@@ -31,4 +31,9 @@ func solution(_ n:Int) -> Int {
     } else {
         return n
     }
+}
+
+// 보완한 코드
+func solution6_2(_ n:Int) -> Int {
+    return n != 0 ? (1...n).filter{ n % $0 == 0 }.reduce(0, +) : 0
 }
