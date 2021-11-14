4c4fdfef965ffd33f38c2ebf7fc4d82dd4175a15
hyenyoon <hyenyoon.j@gmail.com>
2021-11-14 11:30:51 +0900

    연습문제2 변수 colNum 추가

--- 알고리즘 211112/알고리즘 211112/2. 크레인 인형뽑기 게임.swift
+++ 알고리즘 211112/알고리즘 211112/2. 크레인 인형뽑기 게임.swift
@@ -10,29 +10,28 @@ import Foundation
 // 처음 작성한 코드
 func solution2_1(_ board:[[Int]], _ moves:[Int]) -> Int {
     let numOfCol = board.count
-    var colNum = Array(repeating: 0, count: numOfCol)
+    var colNumArr = Array(repeating: 0, count: numOfCol)
+    var colNum = 0
     var pickedDolls = [Int]()
     var deletedDolls = 0
     
     for move in moves {
-        if colNum[move-1] == numOfCol {
+        if colNumArr[move-1] == numOfCol {
             continue
-        }
-        if board[colNum[move-1]][move-1] == 0 {
-            while board[colNum[move-1]][move-1] == 0 {
-                colNum[move-1] += 1
-                if colNum[move-1] == numOfCol {
+        } else { colNum = colNumArr[move-1] }
+        if board[colNum][move-1] == 0 {
+            while board[colNum][move-1] == 0 {
+                if colNum+1 == numOfCol {
+                    colNumArr[move-1] = colNum+1
                     break
-                }
+                } else { colNum += 1 }
             }
         }
-        if board[colNum[move-1]][move-1] == pickedDolls.last {
-            pickedDolls.removeLast(1)
+        if board[colNum][move-1] == pickedDolls.last {
+            pickedDolls.removeLast()
             deletedDolls += 2
-        } else {
-            pickedDolls.append(board[colNum[move-1]][move-1])
-        }
-        colNum[move-1] += 1
+        } else { pickedDolls.append(board[colNum][move-1]) }
+        colNumArr[move-1] = colNum + 1
     }
     return deletedDolls
 }
