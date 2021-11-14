394f474ea88bfe011e3bd315fa393e6828e4b5ae
hyenyoon <hyenyoon.j@gmail.com>
2021-11-15 02:06:30 +0900

    문제5 코드보완

--- 알고리즘 211112/알고리즘 211112/5. 2016년.swift
+++ 알고리즘 211112/알고리즘 211112/5. 2016년.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 // 처음 작성한 코드
-func solution(_ a:Int, _ b:Int) -> String {
+func solution5_1(_ a:Int, _ b:Int) -> String {
     let daysofMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     let daysOfWeek = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"]
     var days = 0
@@ -18,4 +18,14 @@ func solution(_ a:Int, _ b:Int) -> String {
     }
     
     return daysOfWeek[(b - 1 + days) % 7]
+}
+
+// 다른 사람의 풀이 확인 후 보완한 코드
+
+func solution5_2(_ a:Int, _ b:Int) -> String {
+    let daysofMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    let daysOfWeek = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"] // THU를 앞으로 두어 b-1 -> b
+    let days = daysofMonth[0..<a-1].reduce(0, +) + b
+    
+    return daysOfWeek[days % 7]
 }
---
+++ 알고리즘 211112/알고리즘 211112/6. 약수의 합.swift
@@ -0,0 +1,8 @@
+//
+//  6. 약수의 합.swift
+//  알고리즘 211112
+//
+//  Created by Hyenyoon Jung on 2021/11/15.
+//
+
+import Foundation
