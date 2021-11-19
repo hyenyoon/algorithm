882c0fc3be6e24de6861b58d19a79fabc285caca
hyenyoon <hyenyoon.j@gmail.com>
2021-11-19 16:29:38 +0900

    Q4 solved

---
+++ HackerRank/HackerRank/aVeryBigSum.swift
@@ -0,0 +1,16 @@
+//
+//  aVeryBigSum.swift
+//  HackerRank
+//
+//  Created by Hyenyoon Jung on 2021/11/19.
+//
+
+import Foundation
+
+func aVeryBigSum(ar: [Int]) -> Int {
+    var result = 0
+    for n in ar {
+        result += n
+    }
+    return result
+}
