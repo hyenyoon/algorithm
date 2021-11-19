280a14ff5fc537c4032e683b924d73bbd938d7e9
hyenyoon <hyenyoon.j@gmail.com>
2021-11-19 15:24:55 +0900

    Q1 Solved

---
+++ HackerRank/HackerRank/SolveMeFirst.swift
@@ -0,0 +1,16 @@
+//
+//  Solve Me First.swift
+//  HackerRank
+//
+//  Created by Hyenyoon Jung on 2021/11/19.
+//
+
+import Foundation
+
+func sumTwoNumbers() {
+    guard let a = Int(readLine()!), let b = Int(readLine()!) else {
+        return
+    }
+    print(a+b)
+}
+
---
+++ HackerRank/HackerRank/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  HackerRank
+//
+//  Created by Hyenyoon Jung on 2021/11/19.
+//
+
+import Foundation
+
+sumTwoNumbers()
