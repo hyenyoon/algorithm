560af2263879add02687c2cbc2f2b890c6d1c3d5
hyenyoon <hyenyoon.j@gmail.com>
2021-11-26 11:42:59 +0900

    isPalindrome Solved

---
+++ leetcode/leetcode/9. isPalindrome.swift
@@ -0,0 +1,18 @@
+//
+//  9. isPalindrome.swift
+//  leetcode
+//
+//  Created by Hyenyoon Jung on 2021/11/26.
+//
+
+import Foundation
+
+func isPalindrome(_ x: Int) -> Bool {
+    var reversed = ""
+    
+    for char in String(x).reversed() {
+        reversed.append(char)
+    }
+    
+    return String(x) == reversed
+}
