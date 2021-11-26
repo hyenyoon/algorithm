599791404a974af466648081ab18e72a471ce61e
hyenyoon <hyenyoon.j@gmail.com>
2021-11-26 20:57:06 +0900

    convert to Loop working on

--- leetcode/leetcode/21. Merge Two Sorted Lists.swift
+++ leetcode/leetcode/21. Merge Two Sorted Lists.swift
@@ -17,7 +17,7 @@ public class ListNode {
 
 // https://leetcode.com/problems/merge-two-sorted-lists/discuss/1132854/Swift%3A-Merge-Two-Sorted-Lists-%2B-Tests
 
-func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+func mergeTwoListsR(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
     guard let l1 = list1 else {
         return list2
     }
@@ -26,11 +26,47 @@ func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
     }
 
     if l1.val < l2.val {
-        l1.next = mergeTwoLists(l1.next, l2)
+        l1.next = mergeTwoListsR(l1.next, l2)
         return l1
     } else {
-        l2.next = mergeTwoLists(l1, l2.next)
+        l2.next = mergeTwoListsR(l1, l2.next)
         return l2
     }
 }
 
+
+// convert to Loop
+//func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+//    if list1 == nil { return list2 }
+//    if list2 == nil { return list1 }
+//
+//    var l1 = list1
+//    var l2 = list2
+//
+//    var list = [ListNode]()
+//
+//    while l1 != nil || l2 != nil {
+//        if l1 == nil {
+//            list.append(l2!)
+//            l2 = l2!.next
+//        }
+//        if l2 == nil {
+//            list.append(l1!)
+//            l1 = l1!.next
+//        }
+//
+//        if l1!.val < l2!.val {
+//            list.append(l1!)
+//            l1 = l1!.next
+//        } else {
+//            list.append(l2!)
+//            l2 = l2!.next
+//        }
+//    }
+//
+//    for i in 0 ..< list.count - 1 {
+//        list[i].next = list[i+1]
+//    }
+//
+//    return list[0]
+//}
--- leetcode/leetcode/main.swift
+++ leetcode/leetcode/main.swift
@@ -7,10 +7,20 @@
 
 import Foundation
 
-
-//print(romanToInt("IX"))
-
-
-print(longestCommonPrefix(["",""]))
-
+//let a_1 = ListNode(1)
+//let a_2 = ListNode(3)
+//let a_3 = ListNode(5)
+//a_1.next = a_2
+//a_1.next = a_3
+//
+//
+//let b_1 = ListNode(2)
+//let b_2 = ListNode(4)
+//let b_3 = ListNode(6)
+//b_1.next = b_2
+//b_2.next = b_3
+//
+////print(a_1.val, a_1.next)
+//
+//mergeTwoLists(a_1, b_1)
 
