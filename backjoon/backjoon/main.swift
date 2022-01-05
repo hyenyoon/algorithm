2ff26acee701054a240eec06cd183549321a816f
hyenyoon <hyenyoon.j@gmail.com>
2022-01-05 15:27:32 +0900

    1076번 저항문제 해결

--- backjoon/backjoon/main.swift
+++ backjoon/backjoon/main.swift
@@ -1,12 +1,18 @@
 
 import Foundation
 
-(1...Int(readLine()!)!).forEach { l in
-    let input = readLine()!
-        .split { $0 == " " }
-        .map { Int($0)! }
-    let a = input[0] % 10
-    let b = input[1] % 4 == 0 ? 4 : input[1] % 4
-    let result = Int(pow(Double(a), Double(b))) % 10
-    print(result == 0 ? 10 : result)
-}
+let dic = [
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9
+]
+
+let power: (Int, Int) -> Int = { $0 == 0 ? $1 : power($0 - 1, $1 * 10) }
+print((dic[readLine()!]! * 10 + dic[readLine()!]!) * power(dic[readLine()!]!, 1))
