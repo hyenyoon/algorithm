177a2348274f97a303f715f1a14e447e5a655bd5
hyenyoon <hyenyoon.j@gmail.com>
2022-01-12 15:26:17 +0900

    [83] 주석처리

--- leetcode/leetcode/83. RemoveDuplicatesFromSortedList.swift
+++ leetcode/leetcode/83. RemoveDuplicatesFromSortedList.swift
@@ -7,22 +7,22 @@
 
 import Foundation
 
-public class ListNode {
-    public var val: Int
-    public var next: ListNode?
-    public init() { self.val = 0; self.next = nil; }
-    public init(_ val: Int) { self.val = val; self.next = nil; }
-    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
-}
+//public class ListNode {
+//    public var val: Int
+//    public var next: ListNode?
+//    public init() { self.val = 0; self.next = nil; }
+//    public init(_ val: Int) { self.val = val; self.next = nil; }
+//    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+//}
 
-func deleteDuplicates(_ head: ListNode?) -> ListNode? {
-    var curr = head
-    while curr != nil {
-        if curr?.val == curr?.next?.val {
-            curr?.next = curr?.next?.next
-        } else {
-            curr = curr?.next
-        }
-    }
-    return head
-}
+//func deleteDuplicates(_ head: ListNode?) -> ListNode? {
+//    var curr = head
+//    while curr != nil {
+//        if curr?.val == curr?.next?.val {
+//            curr?.next = curr?.next?.next
+//        } else {
+//            curr = curr?.next
+//        }
+//    }
+//    return head
+//}
